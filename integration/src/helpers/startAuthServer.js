316ec3229c0bd4ab20fd83d905cccf34a631f9a4
Tobias Lindström <me@tobiaslindstrom.se>
2022-07-02 17:17:26 +0200

    Added linting to the CI.

--- integration/src/helpers/startAuthServer.js
+++ integration/src/helpers/startAuthServer.js
@@ -2,6 +2,7 @@ const { spawn } = require('child_process')
 const axios = require('axios')
 const getPort = require('get-port')
 
+// eslint-disable-next-line no-async-promise-executor, consistent-return
 const startAuthServer = async (extendedEnv) => new Promise(async (resolve, reject) => {
   const port = await getPort()
 
@@ -10,7 +11,9 @@ const startAuthServer = async (extendedEnv) => new Promise(async (resolve, rejec
     .on('exit', (code) => reject(code))
 
   if (process.env.DEBUG) {
+    // eslint-disable-next-line no-console
     server.stdout.on('data', (data) => console.log(`stdout: ${data}`))
+    // eslint-disable-next-line no-console
     server.stderr.on('data', (data) => console.error(`stderr: ${data}`))
   }
 
@@ -18,12 +21,15 @@ const startAuthServer = async (extendedEnv) => new Promise(async (resolve, rejec
 
   while (!server.exitCode) {
     try {
+      // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
       const { status } = await axios(`${url}/readyz`)
 
       if (status === 200) {
+        // eslint-disable-next-line no-promise-executor-return
         return resolve({ url, server })
       }
     } catch (err) {
+      // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
       await new Promise((resolveWait) => setTimeout(resolveWait, 50))
     }
   }
